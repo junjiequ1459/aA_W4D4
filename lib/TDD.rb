241281479e4de24845a2cf09eecf82a8211a554f
chakhoic <chakhoichan@gmail.com>
2022-12-01 17:06:18 -0500

    finished game

--- lib/TDD.rb
+++ lib/TDD.rb
@@ -37,5 +37,63 @@ def stock_picker(arr)
   result
 end
 
-p stock_picker([10, 9.5, 12, 6, 30, 2])
+# p stock_picker([10, 9.5, 12, 6, 30, 2])
 # [10,9.5,12,6,30,2] => [3,4]
+
+class TowersOfHanoi
+  attr_accessor :arr1, :arr2, :arr3
+
+  def initialize
+    @arr1 = [3, 2, 1]
+    @arr2 = []
+    @arr3 = []
+  end
+
+  def play
+    while !won?
+      p "please enter a number"
+
+      num1 = gets.chomp.to_i
+
+      p "please enter another number"
+      num2 = gets.chomp.to_i
+
+      move(num1, num2)
+    end
+  end
+
+  def move(num1, num2)
+    if num1 == 1
+      cur = arr1
+    elsif num1 == 2
+      cur = arr2
+    elsif num1 == 3
+      cur = arr3
+    end
+
+    if num2 == 1
+      nex = arr1
+    elsif num2 == 2
+      nex = arr2
+    elsif num2 == 3
+      nex = arr3
+    end
+
+    if nex.empty?
+      nex << cur[-1]
+      cur.pop
+    elsif !nex.empty? && nex[-1] > cur[-1]
+      nex << cur[-1]
+      cur.pop
+    end
+    p arr1
+    p arr2
+    p arr3
+  end
+
+  def won?
+    p "you win"
+    return true if arr3 == [3, 2, 1]
+    false
+  end
+end
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -32,3 +32,24 @@ describe "stock_picker" do
     expect(stock_picker([10, 9.5, 12, 6, 30, 2])).to eq([3, 4])
   end
 end
+
+describe TowersOfHanoi do
+  let (:tower) { TowersOfHanoi.new }
+  describe "won?" do
+    it "return true if user won" do
+      tower.arr3 = [3, 2, 1]
+      expect(tower.won?).to eq true
+    end
+  end
+  describe "move" do
+    it "should allow player to move" do
+      tower.move(tower.arr1, tower.arr3)
+      expect(tower.arr3).to eq([1])
+    end
+    it "should not allow player to make illegal move" do
+      tower.move(tower.arr1, tower.arr3)
+      tower.move(tower.arr1, tower.arr3)
+      expect(tower.arr3).to eq([1])
+    end
+  end
+end
